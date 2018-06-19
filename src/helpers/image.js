9c6e45614c447098c47b53189339d511706c6a40
kaptinlin <kaptinlin@gmail.com>
2018-06-19 23:41:59 +0800

    add image helper

--- src/helpers/image.js
+++ src/helpers/image.js
@@ -16,63 +16,130 @@ const sourcePath = globParent(config.images.source);
 const buildPath = config.images.build;
 const relativePath = path.relative(config.paths.build, config.images.build);
 
-const generatePicture = (url, src, srcset, webp, placeholder, attributes) => {
-  const source = path.join(sourcePath, url);
-  const {ext, dir, name, base} = path.parse(url);
+const processImage = (image) => {
+  let match = /\S+@(\d+)\.\w+/.exec(image);
+  let width, file = image;
+
+  if(match) {
+    width = parseInt(match[1]);
+    image = match[0];
+  }
+
+  const dist = path.join(config.paths.build, image);
+  if(!fs.existsSync(dist)) {
+    let processor = sharp(source);
+    if(width) {
+      processor.resize(width);
+    }
+
+    processor.toBuffer().then(data => {
+      imagemin.buffer(data, config.images.build, {
+        plugins: [
+          mozJpegPlugin({ progressive: true }),
+          pngquantPlugin(),
+          gifLossyPlugin(),
+          webpPlugin(),
+          svgoPlugin({
+            plugins: [
+              {
+                removeViewBox: true
+              }
+            ]
+          })
+        ]
+      }).then(data => {
+        fs.ensureFileSync(dist);
+        fs.writeFileSync(dist, data);
+      });
+    });
+  }
+}
+
+const parseSizes = (sizes) => {
+  if(sizes) {
+    sizes = sizes.split(',');
+  } else {
+    sizes = [];
+  }
+
+  let sizeMap = [];
+  let screenMap = [];
+  let maxSize = null;
+
+  sizes.forEach(function(size) {
+    size = size.trim();
+
+    if(/^\d+$/.test(size)) {
+      sizeMap.push({
+        width: parseInt(size, 10),
+        density: `${size}w`
+      });
+    } else if(/^\d+\s+[a-z]+$/.test(size)) {
+      const found = size.match(/^(\d+)\s+([a-z]+)$/);
+      let screen = found[2];
+      let density = found[2];
+
+      if(config.breakpoints.hasOwnProperty(screen)){
+        screenMap.push({
+          width: parseInt(found[1], 10),
+          screen: config.breakpoints[screen]
+        });
+      }
+    } else if(/^\d+\s+\d+$/.test(size)) {
+      const found = size.match(/^(\d+)\s+(\d+)$/);
+
+      sizeMap.push({
+        width: parseInt(found[1], 10),
+        density: `${found[2]}w`
+      });
+    } else if(/^\d+\s+\d+w$/.test(size)) {
+      const found = size.match(/^(\d+)\s+(\d+w)$/);
+
+      sizeMap.push({
+        width: parseInt(found[1], 10),
+        density: found[2]
+      });
+    }
+  });
+
+  sizeMap.sort(function(a, b) {
+    return a.width - b.width;
+  });
+  screenMap.sort(function(a, b) {
+    return a.screen - b.screen;
+  });
+
+  if(screenMap.length > 0) {
+    maxSize = screenMap[screenMap.length - 1].width;
+  } else if(sizeMap.length > 0) {
+    maxSize = sizeMap[sizeMap.length - 1].width;
+  }
+
+  return {
+    sizeMap: sizeMap,
+    screenMap: screenMap,
+    maxSize: maxSize
+  }
+}
+
+const generateSrcset = (basepath, sizeMap, ext) => {
+  return sizeMap.map(item => {
+    return `${relativePath}/${basepath}@${item.width}${ext} ${item.density}`;
+  }).join(', ');
+}
+
+const generatePicture = (src, size, sizes, webp, placeholder, attributes) => {
+  const source = path.join(sourcePath, src);
+  const {ext, dir, name, base} = path.parse(src);
 
   let basepath = name;
   if(dir) {
     basepath = `${dir}/${name}`;
   }
 
-  const processImage = (image) => {
-    let match = /\S+@(\d+)\.\w+/.exec(image);
-    let width, file = image;
-
-    if(match) {
-      width = parseInt(match[1]);
-      image = match[0];
-    }
-
-    const dist = path.join(config.paths.build, image);
-    if(!fs.existsSync(dist)) {
-      let processor = sharp(source);
-      if(width) {
-        processor.resize(width);
-      }
-
-      processor.toBuffer().then(data => {
-        imagemin.buffer(data, config.images.build, {
-          plugins: [
-            mozJpegPlugin({ progressive: true }),
-            pngquantPlugin(),
-            gifLossyPlugin(),
-            webpPlugin(),
-            svgoPlugin({
-              plugins: [
-                {
-                  removeViewBox: true
-                }
-              ]
-            })
-          ]
-        }).then(data => {
-          fs.ensureFileSync(dist);
-          fs.writeFileSync(dist, data);
-        });
-      });
-    }
-  }
-
   const generateImgTag = (src, attributes) => {
     processImage(src);
     return `<img data-src="${src}"${stringifyAttributes(attributes)} />`;
-  }
-
-  const generateSrcSet = (basepath, srcMap, ext) => {
-    return srcMap.map(candidate => {
-      return `${relativePath}/${basepath}@${candidate.width}${ext} ${candidate.density}`;
-    }).join(', ');
   }
 
   const generateSourceTag = (srcset, type, media) => {
@@ -95,98 +162,41 @@ const generatePicture = (url, src, srcset, webp, placeholder, attributes) => {
   }
 
   const generateScreenSourceTags = (basepath, screenMap, ext, webp = false) => {
-    return screenMap.map((candidate, index) => {
+    return screenMap.map((item, index) => {
       let output, media, type;
 
       if(index === 0 && screenMap.length === 1) {
-
         media = `(min-width: ${next}px)`;
       } else if(index < screenMap.length - 1){
         let next = screenMap[index+1].screen - 1;
-        if(candidate.screen == 0) {
+        if(item.screen == 0) {
           media = `(max-width: ${next}px)`;
         } else {
-          media = `(min-width: ${candidate.screen}px) and (max-width: ${next}px)`;
+          media = `(min-width: ${item.screen}px) and (max-width: ${next}px)`;
         }
       } else {
-        media = `(min-width: ${candidate.screen}px)`;
+        media = `(min-width: ${item.screen}px)`;
       }
 
       if(webp) {
         type = "image/webp";
       }
 
-      return generateSourceTag(`${relativePath}/${basepath}@${candidate.width*2}${ext} 2x, ${relativePath}/${basepath}@${candidate.width}${ext} 1x`, type, media);
+      return generateSourceTag(`${relativePath}/${basepath}@${item.width*2}${ext} 2x, ${relativePath}/${basepath}@${item.width}${ext} 1x`, type, media);
     }).join('');
   }
 
-  if(srcset) {
-    srcset = srcset.split(',');
-  } else {
-    srcset = [];
-  }
+  const {screenMap, sizeMap, maxSize} = parseSizes(sizes);
 
-  let srcMap = [];
-  let screenMap = [];
-
-  srcset.forEach(function(candidate) {
-    candidate = candidate.trim();
-
-    if(/^\d+$/.test(candidate)) {
-      srcMap.push({
-        width: parseInt(candidate, 10),
-        density: `${candidate}w`
-      });
-    } else if(/^\d+\s+[a-z]+$/.test(candidate)) {
-      const found = candidate.match(/^(\d+)\s+([a-z]+)$/);
-      let screen = found[2];
-      let density = found[2];
-
-      if(config.breakpoints.hasOwnProperty(screen)){
-        screenMap.push({
-          width: parseInt(found[1], 10),
-          screen: config.breakpoints[screen]
-        });
-      }
-    } else if(/^\d+\s+\d+$/.test(candidate)) {
-      const found = candidate.match(/^(\d+)\s+(\d+)$/);
-
-      srcMap.push({
-        width: parseInt(found[1], 10),
-        density: `${found[2]}w`
-      });
-    } else if(/^\d+\s+\d+w$/.test(candidate)) {
-      const found = candidate.match(/^(\d+)\s+(\d+w)$/);
-
-      srcMap.push({
-        width: parseInt(found[1], 10),
-        density: found[2]
-      });
-    }
-  });
-
-  srcMap.sort(function(a, b) {
-    return a.width - b.width;
-  });
-  screenMap.sort(function(a, b) {
-    return a.screen - b.screen;
-  });
-
-  if(!src) {
-    if(screenMap.length > 0) {
-      src = screenMap[0].width;
-    } else if(srcMap.length > 0) {
-      src = srcMap[0].width;
-    } else {
-      src = null;
-    }
+  if(!size) {
+    size = maxSize;
   }
 
   let output = '';
 
   if(fs.existsSync(source)) {
-    if(placeholder === 'true' || placeholder === true) {
-      output = `<div class="image"`;
+    if(placeholder !== 'false') {
+      output = `<div class="${placeholder}"`;
       const dimensions = sizeOf(source);
       const padding = 100*(dimensions.height/dimensions.width);
 
@@ -197,12 +207,12 @@ const generatePicture = (url, src, srcset, webp, placeholder, attributes) => {
     if(webp === 'true' || webp === true) {
       output += generateScreenSourceTags(basepath, screenMap, '.webp', true);
 
-      if(srcMap.length > 0) {
-        output += generateSourceTag(generateSrcSet(basepath, srcMap, '.webp'), 'image/webp');
+      if(sizeMap.length > 0) {
+        output += generateSourceTag(generateSrcset(basepath, sizeMap, '.webp'), 'image/webp');
       }
 
-      if(src !== null) {
-        output += generateSourceTag(`${relativePath}/${basepath}@${src}.webp`, 'image/webp');
+      if(size !== null) {
+        output += generateSourceTag(`${relativePath}/${basepath}@${size}.webp`, 'image/webp');
       } else {
         output += generateSourceTag(`${relativePath}/${basepath}.webp`, 'image/webp');
       }
@@ -210,33 +220,131 @@ const generatePicture = (url, src, srcset, webp, placeholder, attributes) => {
 
     output += generateScreenSourceTags(basepath, screenMap, ext);
 
-    if(srcMap.length > 0) {
-      output += generateSourceTag(generateSrcSet(basepath, srcMap, ext));
+    if(sizeMap.length > 0) {
+      output += generateSourceTag(generateSrcset(basepath, sizeMap, ext));
     }
 
-    if(src) {
-      output += generateImgTag(`${relativePath}/${basepath}@${src}${ext}`, attributes);
+    if(size) {
+      output += generateImgTag(`${relativePath}/${basepath}@${size}${ext}`, attributes);
     } else {
-      output += generateImgTag(`${relativePath}/${url}`, attributes);
+      output += generateImgTag(`${relativePath}/${src}`, attributes);
     }
 
     output += '</picture>';
 
-    if(placeholder === 'true' || placeholder === true) {
+    if(placeholder !== 'false') {
       output += '</div>';
     }
   } else {
-    output = generateImgTag(`${relativePath}/${url}`, attributes);
+    output = generateImgTag(`${relativePath}/${src}`, attributes);
+  }
+
+  return output;
+}
+
+const generateSizesFromScreen = (screenMap) => {
+  return screenMap.map((item, index) => {
+    let output, media, type;
+
+    if(index === 0 && screenMap.length === 1) {
+      media = `(min-width: ${next}px)`;
+    } else if(index < screenMap.length - 1){
+      let next = screenMap[index+1].screen - 1;
+      if(item.screen == 0) {
+        media = `(max-width: ${next}px)`;
+      } else {
+        media = `(min-width: ${item.screen}px) and (max-width: ${next}px)`;
+      }
+    } else {
+      media = `(min-width: ${item.screen}px)`;
+    }
+
+    return `${media} ${item.width}px`;
+  }).join(', ');
+}
+
+const generateImage = (src, size, sizes, placeholder, attributes) => {
+  const source = path.join(sourcePath, src);
+  const {ext, dir, name, base} = path.parse(src);
+
+  let basepath = name;
+  if(dir) {
+    basepath = `${dir}/${name}`;
+  }
+
+  let {screenMap, sizeMap, maxSize} = parseSizes(sizes);
+
+  screenMap.forEach((item) => {
+    sizeMap.push({
+      width: item.width,
+      density: `${item.width}w`
+    });
+  });
+
+  sizeMap.sort(function(a, b) {
+    return a.width - b.width;
+  });
+
+  if(!size) {
+    size = maxSize;
+  }
+
+  let output = '';
+
+  if(fs.existsSync(source)) {
+    if(placeholder !== 'false') {
+      output = `<div class="${placeholder}"`;
+      const dimensions = sizeOf(source);
+      const padding = 100*(dimensions.height/dimensions.width);
+
+      output += ` style="padding-bottom: ${padding.toFixed(3)}%">`;
+    }
+    output += '<img';
+
+    if(sizeMap.length > 0) {
+      output += ` data-srcset="${generateSrcset(basepath, sizeMap, ext)}"`
+    }
+    if(size) {
+      output += ` data-src="${relativePath}/${basepath}@${size}${ext}"`;
+    } else {
+      output += ` data-src="${relativePath}/${src}"`;
+    }
+    if(screenMap.length > 0) {
+      output += ` sizes="${generateSizesFromScreen(screenMap)}"`;
+    }
+
+    output += ` ${stringifyAttributes(attributes)} />`;
+
+    if(placeholder !== 'false') {
+      output += '</div>';
+    }
   }
 
   return output;
 }
 
 module.exports.register = function (Handlebars) {
-  Handlebars.registerHelper("image", function(url, options) {
-    const {webp = true, src = null, srcset = null, placeholder = true, ...attributes} = options.hash || {};
+  /**
+   * {{ picture "test.png" placeholder="image" size="640" sizes="640 xs, 960 md" class="lazy" alt="Test image" }}
+   * {{ picture "test.png" placeholder="image" size="640" sizes="640, 960" class="lazy" alt="Test image" }}
+   * {{ picture "test.png" placeholder="image" size="640" sizes="640 640w, 960 960w" class="lazy" alt="Test image" }}
+   **/
+  Handlebars.registerHelper("picture", function(src, options) {
+    const {webp = true, size = null, sizes = null, placeholder = 'image', ...attributes} = options.hash || {};
 
-    const output = generatePicture(url, src, srcset, webp, placeholder, attributes);
+    const output = generatePicture(src, size, sizes, webp, placeholder, attributes);
+    return new Handlebars.SafeString(output);
+  });
+
+  /**
+   * {{ image "test.png" placeholder="image" sizes="640 xs, 960 md" class="lazy" alt="Test image" }}
+   * {{ image "test.png" placeholder="image" size="640" sizes="640, 960" class="lazy" alt="Test image" }}
+   * {{ image "test.png" placeholder="image" size="640" sizes="640 640w, 960 960w" class="lazy" alt="Test image" }}
+   **/
+  Handlebars.registerHelper("image", function(src, options) {
+    const {size = null, sizes = null, placeholder = 'image', ...attributes} = options.hash || {};
+
+    const output = generateImage(src, size, sizes, placeholder, attributes);
     return new Handlebars.SafeString(output);
   });
 };
